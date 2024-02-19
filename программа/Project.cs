06e562218839b9a94a523cb84c505dcda9553a7e
unknown <vip.melnikov2003@mail.ru>
2024-02-19 19:39:43 +0300

    Изменение файла Project.cs

--- программа/Project.cs
+++ программа/Project.cs
@@ -1,6 +1,6 @@
 /* Проект */
 public class Project{
-	string name; //название
+	string caption; //наименование
 	string author; //автор
 	uint size = 0; //размер в байтах
 	List<Item> items = new List<Item>(); //объекты в проекте
@@ -9,5 +9,15 @@ public class Project{
 	public Project(string name, string author){
 		this.name = name;
 		this.author = author;
+	}
+	
+	//добавить объект в проект
+	public void AddItem(Item item){
+		items.Add(item);
+	}
+	
+	//удалить объект в проекте
+	public void AddItem(Item item){
+		items.Remove(item);
 	}
 }
