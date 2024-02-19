7fcbf47d1a396a453ed9cbcf9c6bc64accc01398
unknown <vip.melnikov2003@mail.ru>
2024-02-19 19:43:52 +0300

    Добавление файла Item.cs

---
+++ программа/Item.cs
@@ -0,0 +1,23 @@
+/* Объект (абстрактный) */
+public abstract class Item{
+	
+	Point position; //позиция x, y, z
+	Size size; //размер объекта (длина, ширина, высота)
+	Rotation rotation; // углы поворота объекта в градусах (по x, y и z)
+	
+	/* Изменение позиции объекта */
+	public void ChangePosition(Point position){
+		this.position = position;
+	}
+	
+	/* Изменение размера объекта */
+	public void ChangeSize(Size size){
+		this.size = size;
+	}
+	
+	/* Изменение угла поворота объекта */
+	public void ChangeRotate(Rotation rotation){
+		this.rotation = rotation;
+	}
+	
+}
